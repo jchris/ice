47a76a8ffb9ebf75754bf13ecbfa677f615a8854
Chris Anderson <jchris@apache.org>
2010-05-03 19:05:10 -0700

    fix for url escaping issue

--- evently/editor/evently/data.js
+++ evently/editor/evently/data.js
@@ -1,8 +1,6 @@
 function(e, params) {
   var ddoc = e.data.args[0], name = params.name;
   return {
-    name : name,
-    map : ddoc.views[name].map,
-    reduce : ddoc.views[name].reduce
+    name : name
   }
 };
