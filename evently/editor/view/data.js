1110e90534bf61798654be69213311ffe5ff488b
Chris Anderson <jchris@apache.org>
2010-05-07 10:00:45 -0700

    bespin code updates from parent doc

--- evently/editor/view/data.js
+++ evently/editor/view/data.js
@@ -2,6 +2,7 @@ function(e, params) {
   var ddoc = $$(this).app.doc, name = params.name;
   return {
     name : name,
-    docid : ddoc._id
+    docid : ddoc._id,
+    rand : Math.random().toString().split(".").pop()
   };
 };
--- evently/editor/view/selectors/iframe/load.js
+++ evently/editor/view/selectors/iframe/load.js
@@ -1,10 +1,12 @@
 function(e) {
-  $.log("onload iframe",this, arguments)
   var name = this.name, ddoc = $$(this).app.doc;
   var view = e.data.args[1].name;
-  var node = window[name].document.getElementById("edit");
-  node.bespin.addEventListener('textChange', function() {
-    ddoc.views[view][name] = window[name].getCode();
-    // show the save button
+  var vtype = name.split('.').pop();
+  $.log("onload iframe",  view, name);
+  window[name].SC.ready(function() {
+    window[name].setCode(ddoc.views[view][vtype]||"");
+    window[name].onCodeChange(function(code) {
+      ddoc.views[view][vtype] = code;
+    });
   });
 };
