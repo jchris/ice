ccefb63f94a561f9179bb1eb2375f3a6d553a5bd
Chris Anderson <jchris@apache.org>
2010-05-10 13:25:12 -0700

    link to evently content

--- evently/editor/evently/data.js
+++ evently/editor/evently/data.js
@@ -1,17 +1,18 @@
 function(e, params) {
-  var events = [], ddoc = $$(this).app.doc, name = params.name;
-  $.forIn(ddoc.evently[name], function(k, v) {
-    events.push({
-      event : k
-    });
-  });
-  $.log(events)
+  var events = [], ddoc = $$(this).app.doc, ps = params.path.split('.');
+  var t, c = ddoc.evently;
+  ps.forEach(function(p) {t = c; c = c[p];});
+  // $.forIn(ddoc.evently[name], function(k, v) {
+  //   events.push({
+  //     event : k
+  //   });
+  // });
+  // $.log(events)
   var design = $$(this).app.require("lib/design");
   var v = {
     name : name,
-    evently : design.evently(ddoc.evently[name]),
-    path : ["evently",name,"event"].join('/')
+    fields : design.eventlyCodes(c)
   };
-  $.log(v);
+  // $.log(v);
   return v;
 };
--- evently/editor/view/data.js
+++ evently/editor/view/data.js
@@ -1,4 +1,4 @@
 function(e, params) {
   var ddoc = $$(this).app.doc, name = params.name;
-  return {name : name};
+  return {name : name, map : ddoc.views[name].map, reduce : ddoc.views[name].reduce};
 };
